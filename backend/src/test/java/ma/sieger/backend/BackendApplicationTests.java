43dfc3b64f990d72f3db6ae9306e73ea68be5ded
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-10-24 10:45:34 +0100

    add JWT dependencies and create backend package structure

---
+++ backend/src/test/java/ma/sieger/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.sieger.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
