16bff2b230e1e6aa1e1dd31760201c6c41b5df03
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-10-27 15:36:48 +0100

    installing react-router-dom, and axios, and create login and hello routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  
 import LoginPage from './pages/LoginPage'
 import HelloPage from './pages/HelloPage'
 
 function App() {
 
   return (
-    <>
-      {/* <LoginPage /> */}
-      <HelloPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/hello" element={<HelloPage />} />
+      </Routes>
+    </Router>
   )
 }
 
